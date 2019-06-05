e6aaf9265f48ffcb20286b8a810e997150063d9b
Quentin Hébert <q.hebert@dental-monitoring.com>
2019-06-05 17:56:14 +0200

    ContinuousSurfaceReader: YUV_420_888 byte array as output

--- fotoapparat/src/main/java/io/fotoapparat/hardware/v2/readers/ContinuousSurfaceReader.java
+++ fotoapparat/src/main/java/io/fotoapparat/hardware/v2/readers/ContinuousSurfaceReader.java
@@ -1,6 +1,7 @@
 package io.fotoapparat.hardware.v2.readers;
 
 import android.graphics.ImageFormat;
+import android.graphics.Rect;
 import android.media.Image;
 import android.media.ImageReader;
 import android.os.Build;
@@ -31,24 +32,78 @@ public class ContinuousSurfaceReader
         this.cameraThread = cameraThread;
     }
 
-    private static byte[] YUV_420_888toNV21(Image image) {
-        byte[] nv21;
-        ByteBuffer yBuffer = image.getPlanes()[0].getBuffer();
-        ByteBuffer uBuffer = image.getPlanes()[1].getBuffer();
-        ByteBuffer vBuffer = image.getPlanes()[2].getBuffer();
+    private byte[] imageToByte(final Image image)
+    {
+        final Rect crop   = image.getCropRect();
+        final int  width  = crop.width();
+        final int  height = crop.height();
 
-        int ySize = yBuffer.remaining();
-        int uSize = uBuffer.remaining();
-        int vSize = vBuffer.remaining();
+        final Image.Plane[] planes     = image.getPlanes();
+        final byte[]        rowData    = new byte[planes[0].getRowStride()];
+        final int           bufferSize = width * height * ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888) / 8;
+        final ByteBuffer    output     = ByteBuffer.allocateDirect(bufferSize);
 
-        nv21 = new byte[ySize + uSize + vSize];
+        int channelOffset = 0;
+        int outputStride = 0;
 
-        //U and V are swapped
-        yBuffer.get(nv21, 0, ySize);
-        vBuffer.get(nv21, ySize, vSize);
-        uBuffer.get(nv21, ySize + vSize, uSize);
+        for (int planeIndex = 0; planeIndex < 3; planeIndex++)
+        {
+            if (planeIndex == 0)
+            {
+                channelOffset = 0;
+                outputStride = 1;
+            }
+            else if (planeIndex == 1)
+            {
+                channelOffset = width * height + 1;
+                outputStride = 2;
+            }
+            else if (planeIndex == 2)
+            {
+                channelOffset = width * height;
+                outputStride = 2;
+            }
 
-        return nv21;
+            final ByteBuffer buffer      = planes[planeIndex].getBuffer();
+            final int        rowStride   = planes[planeIndex].getRowStride();
+            final int        pixelStride = planes[planeIndex].getPixelStride();
+
+            final int shift         = (planeIndex == 0) ? 0 : 1;
+            final int widthShifted  = width >> shift;
+            final int heightShifted = height >> shift;
+
+            buffer.position(rowStride * (crop.top >> shift) + pixelStride * (crop.left >> shift));
+
+            for (int row = 0; row < heightShifted; row++)
+            {
+                final int length;
+
+                if (pixelStride == 1 && outputStride == 1)
+                {
+                    length = widthShifted;
+                    buffer.get(output.array(), channelOffset, length);
+                    channelOffset += length;
+                }
+                else
+                {
+                    length = (widthShifted - 1) * pixelStride + 1;
+                    buffer.get(rowData, 0, length);
+
+                    for (int col = 0; col < widthShifted; col++)
+                    {
+                        output.array()[channelOffset] = rowData[col * pixelStride];
+                        channelOffset += outputStride;
+                    }
+                }
+
+                if (row < heightShifted - 1)
+                {
+                    buffer.position(buffer.position() + rowStride - length);
+                }
+            }
+        }
+
+        return output.array();
     }
 
     @Override
@@ -57,7 +112,7 @@ public class ContinuousSurfaceReader
         Image.Plane[] planes = image.getPlanes();
 
         if (planes.length > 0) {
-            byte[] bytes = YUV_420_888toNV21(image);
+            byte[] bytes = imageToByte(image);
 
             if (listener != null) {
                 listener.onFrameAcquired(bytes);
