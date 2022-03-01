7053a47c826c5027c1252af2df15d8b6b9e84d6a
valentin <v.bodereau@smilemate.com>
2022-03-01 11:18:26 +0100

    fix WB

--- fotoapparat/src/main/java/io/fotoapparat/hardware/v2/parameters/Request.java
+++ fotoapparat/src/main/java/io/fotoapparat/hardware/v2/parameters/Request.java
@@ -5,6 +5,7 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.MeteringRectangle;
+import android.hardware.camera2.params.RggbChannelVector;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.Log;
@@ -121,6 +122,10 @@ class Request {
             setPreviewFpsRange();
             setSensorSensitivity();
             setJpegQuality();
+            captureRequest.set(CaptureRequest.CONTROL_AWB_MODE, CaptureRequest.CONTROL_AWB_MODE_OFF);
+            // adjust color correction using seekbar's params
+            captureRequest.set(CaptureRequest.COLOR_CORRECTION_MODE, CaptureRequest.COLOR_CORRECTION_MODE_TRANSFORM_MATRIX);
+            captureRequest.set(CaptureRequest.COLOR_CORRECTION_GAINS, computeTemperature(100));
 
             return captureRequest.build();
         } catch (IllegalStateException e){
@@ -130,6 +135,64 @@ class Request {
                 throw e;
             }
         }
+    }
+
+    private static RggbChannelVector computeTemperature(final int factor)
+    {
+        return new RggbChannelVector(2.0f, 1.0f, 1.0f, 2.0f);
+    }
+
+    public static RggbChannelVector colorTemperature(int whiteBalance) {
+        float temperature = whiteBalance / 100;
+        float red;
+        float green;
+        float blue;
+
+        //Calculate red
+        if (temperature <= 66)
+            red = 255;
+        else {
+            red = temperature - 60;
+            red = (float) (329.698727446 * (Math.pow((double) red, -0.1332047592)));
+            if (red < 0)
+                red = 0;
+            if (red > 255)
+                red = 255;
+        }
+
+
+        //Calculate green
+        if (temperature <= 66) {
+            green = temperature;
+            green = (float) (99.4708025861 * Math.log(green) - 161.1195681661);
+            if (green < 0)
+                green = 0;
+            if (green > 255)
+                green = 255;
+        } else {
+            green = temperature - 60;
+            green = (float) (288.1221695283 * (Math.pow((double) green, -0.0755148492)));
+            if (green < 0)
+                green = 0;
+            if (green > 255)
+                green = 255;
+        }
+
+        //calculate blue
+        if (temperature >= 66)
+            blue = 255;
+        else if (temperature <= 19)
+            blue = 0;
+        else {
+            blue = temperature - 10;
+            blue = (float) (138.5177312231 * Math.log(blue) - 305.0447927307);
+            if (blue < 0)
+                blue = 0;
+            if (blue > 255)
+                blue = 255;
+        }
+
+        return new RggbChannelVector((red / 255) * 2, (green / 255), (green / 255), (blue / 255) * 2);
     }
 
     private void setCaptureIntent() {
